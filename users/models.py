61e9a299bebf7ebe7a469c4acba9541a7be0b92f
Kyeza Arnold <kyezaarnold63@gmail.com>
2020-09-14 05:26:17 +0300

    - added location attribute to user model

--- users/models.py
+++ users/models.py
@@ -75,6 +75,7 @@ class User(AbstractUser, BaseModel):
 
     phone_number = PhoneNumberField(verbose_name='phone number', unique=True, blank=True, null=True)
     user_type = models.CharField(max_length=20, choices=UserTypes.choices, null=True, blank=True)
+    location = models.CharField(max_length=150, null=True, blank=True)
 
     objects = UserManager()
 
