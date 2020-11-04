1d48157bb5ff7bd690e2e86517ecad280ee3019a
Kyeza Arnold <kyezaarnold63@gmail.com>
2020-11-04 05:25:59 +0300

    fixed item image bug

--- main/utils.py
+++ main/utils.py
@@ -2,6 +2,6 @@ from django.utils.text import slugify
 
 
 def get_image_filename(instance, filename):
-    username = instance.user.username
+    username = instance.owner.username
     slug = slugify(username)
-    return f'profile_pics/{slug}-{filename}'
+    return f'item_images/{slug}-{filename}'
